36f6311d47e4d3e70a563e2daea808313d2e2fdb
Ryan Tate <ryan.michael.tate@gmail.com>
2017-10-23 14:45:00 -0700

    initial web worker files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import GitTokenAuctionWorker from './GitTokenAuctionWorker.js'
+import GitTokenAdminWorker from './GitTokenAdminWorker.js'
+import GitTokenVotingWorker from './GitTokenVotingWorker.js'
+
+export {
+  GitTokenAuctionWorker,
+  GitTokenAdminWorker,
+  GitTokenVotingWorker
+}
