b57d2254281088858821f9128ac6621421477b34
Ryan Tate <ryan.michael.tate@gmail.com>
2017-11-02 15:35:24 -0700

    WIP

--- dist/Registry.worker.js
+++ dist/Registry.worker.js
@@ -6,9 +6,25 @@ Object.defineProperty(exports, "__esModule", {
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+var _GitTokenRegistry = require('gittoken-contracts/build/contracts/GitTokenRegistry.json');
+
+var _GitTokenRegistry2 = _interopRequireDefault(_GitTokenRegistry);
+
+var _githubApi = require('github-api');
+
+var _githubApi2 = _interopRequireDefault(_githubApi);
+
+var _axios = require('axios');
+
+var _axios2 = _interopRequireDefault(_axios);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 function _objectDestructuringEmpty(obj) { if (obj == null) throw new TypeError("Cannot destructure undefined"); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// import Web3 from 'web3'
 
 var GitTokenRegistryWorker = function () {
   function GitTokenRegistryWorker(_ref) {
@@ -28,9 +44,15 @@ var GitTokenRegistryWorker = function () {
       addEventListener('message', function (msg) {
         var _JSON$parse = JSON.parse(msg.data),
             event = _JSON$parse.event,
-            values = _JSON$parse.values;
+            payload = _JSON$parse.payload;
 
         switch (event) {
+          case 'verify_organization':
+            return _this.verifyOrganization(payload);
+            break;
+          case 'set_config':
+            return _this.setConfig(payload);
+            break;
           default:
             _this.handleErrorMessage({
               error: 'Invalid Event: ' + event
@@ -39,9 +61,68 @@ var GitTokenRegistryWorker = function () {
       });
     }
   }, {
+    key: 'setConfig',
+    value: function setConfig(_ref2) {
+      var registryAPI = _ref2.registryAPI;
+
+      this.registryAPI = this.registryAPI;
+      postMessage(JSON.stringify({ msg: 'configured' }));
+    }
+  }, {
+    key: 'validateAdmin',
+    value: function validateAdmin(_ref3) {
+      var username = _ref3.username,
+          token = _ref3.token,
+          organization = _ref3.organization;
+
+      return new Promise(function (resolve, reject) {
+        // console.log('username, token, organization', username, token, organization)
+        var github = new _githubApi2.default({ username: username, token: token });
+        var user = github.getUser();
+        var org = github.getOrganization(organization);
+        var profile = void 0;
+        Promise.resolve(user.getProfile()).then(function (_ref4) {
+          var data = _ref4.data;
+
+          profile = data;
+          return org.listMembers({ role: 'admin' });
+        }).then(function (_ref5) {
+          var data = _ref5.data;
+
+          return data;
+        }).map(function (member) {
+          if (member.login == profile.login) {
+            resolve(true);
+          } else {
+            return null;
+          }
+        }).then(function () {
+          resolve(false);
+        }).catch(function (error) {
+          reject(error);
+        });
+      });
+    }
+  }, {
+    key: 'verifyOrganization',
+    value: function verifyOrganization(details) {
+      var _this2 = this;
+
+      var organization = details.organization;
+
+      this.validateAdmin(details).then(function (validated) {
+        console.log('validated', validated);
+        return (0, _axios.post)(_this2.registryAPI + '/verify/' + organization, details);
+      }).then(function (verified) {
+        postMessage(JSON.stringify({ verified: verified }));
+      }).catch(function (error) {
+        console.log('error', error);
+      });
+    }
+  }, {
     key: 'handleErrorMessage',
-    value: function handleErrorMessage(_ref2) {
-      var error = _ref2.error;
+    value: function handleErrorMessage(_ref6) {
+      var error = _ref6.error;
 
       postMessage(JSON.stringify({
         error: error ? error : 'Unhandled Error'
--- src/Registry.worker.js
+++ src/Registry.worker.js
@@ -1,20 +1,73 @@
 import GitTokenRegistry from 'gittoken-contracts/build/contracts/GitTokenRegistry.json'
+import GitHubAPI from 'github-api'
+import axios, { post } from 'axios'
+// import Web3 from 'web3'
 
 export default class GitTokenRegistryWorker {
   constructor({ }) {
+
     this.listen()
   }
 
   listen() {
     console.log('GitToken Registry Web Worker Listening for Events')
     addEventListener('message', (msg) => {
-      const { event, values } = JSON.parse(msg.data)
+      const { event, payload } = JSON.parse(msg.data)
       switch(event) {
+        case 'verify_organization':
+          return this.verifyOrganization(payload)
+          break;
+        case 'set_config':
+          return this.setConfig(payload)
+          break;
         default:
           this.handleErrorMessage({
             error: `Invalid Event: ${event}`
           })
       }
+    })
+  }
+
+  setConfig({ registryAPI }) {
+    this.registryAPI = this.registryAPI
+    postMessage(JSON.stringify({ msg: 'configured' }))
+  }
+
+  validateAdmin({ username, token, organization }) {
+    return new Promise((resolve, reject) => {
+      // console.log('username, token, organization', username, token, organization)
+      const github = new GitHubAPI({ username, token })
+      const user = github.getUser()
+      const org = github.getOrganization(organization)
+      let profile
+      Promise.resolve(user.getProfile()).then(({ data }) => {
+        profile = data
+        return org.listMembers({ role: 'admin' })
+      }).then(({ data }) => {
+        return data
+      }).map((member) => {
+        if (member.login == profile.login) {
+          resolve(true)
+        } else {
+          return null
+        }
+      }).then(() => {
+        resolve(false)
+      }).catch((error) => {
+        reject(error)
+      })
+    })
+  }
+
+  verifyOrganization(details) {
+    const { organization } = details
+    this.validateAdmin(details).then((validated) => {
+      console.log('validated', validated)
+      return post(`${this.registryAPI}/verify/${organization}`, details)
+    }).then((verified) => {
+      postMessage(JSON.stringify({ verified }))
+    }).catch((error) => {
+      console.log('error', error)
     })
   }
 
