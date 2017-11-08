a0d3b2f8111d29f5c7dfde2b9680e051f6da7334
Ryan Tate <ryan.michael.tate@gmail.com>
2017-11-08 14:36:02 -0800

    WIP

--- src/Wallet.worker.js
+++ src/Wallet.worker.js
@@ -2,7 +2,7 @@ import Promise, { promisifyAll } from 'bluebird'
 
 import Tx from 'ethereumjs-tx'
 import { ecsign, sha3 } from 'ethereumjs-util'
-import { keystore, signing } from 'eth-lightwallet'
+import keythereum from 'keythereum'
 import request from 'browser-request'
 import PouchDB from 'pouchdb'
 
@@ -25,24 +25,37 @@ export default class GitTokenWalletWorker {
 
   createKeystore({ password }) {
     return new Promise((resolve, reject) => {
-      keystore.createVault({ password }, (error, ks) => {
-        if (error) { reject(error) }
-        ks.keyFromPassword(password, (error, derivedKey) => {
-          if (error) { reject(error) }
-          ks.generateNewAddress(derivedKey, 3);
-          this.db.bulkDocs([
-            { _id: 'keystore', keystore: ks.serialize() },
-            { _id: 'addresses', addresses: ks.getAddresses() },
-          ]).then(() => {
-            return this.db.get('addresses')
-          }).then((doc) => {
-            console.log('doc', doc)
-            resolve(doc.addresses)
-          }).catch((error) => {
-            reject(error)
+      try {
+        keythereum.create({ keyBytes: 64, ivBytes: 32 }, (dk) => {
+          console.log('dk', dk)
+          keythereum.dump(password, dk.privateKey, dk.salt, dk.iv, {}, (keyObject) => {
+            console.log('keyObject', keyObject)
+            resolve(keyObject)
           })
         })
-      })
+      } catch(error) {
+        console.log('error', error)
+        reject(error)
+      }
+
+      // keystore.createVault({ password }, (error, ks) => {
+      //   if (error) { reject(error) }
+      //   ks.keyFromPassword(password, (error, derivedKey) => {
+      //     if (error) { reject(error) }
+      //     ks.generateNewAddress(derivedKey, 3);
+      //     this.db.bulkDocs([
+      //       { _id: 'keystore', keystore: ks.serialize() },
+      //       { _id: 'addresses', addresses: ks.getAddresses() },
+      //     ]).then(() => {
+      //       return this.db.get('addresses')
+      //     }).then((doc) => {
+      //       console.log('doc', doc)
+      //       resolve(doc.addresses)
+      //     }).catch((error) => {
+      //       reject(error)
+      //     })
+      //   })
+      // })
     })
   }
 
