2aa966289346d6a4b4ca60c75c71acf6f408aa62
Ryan Tate <ryan.michael.tate@gmail.com>
2017-12-13 16:05:35 -0800

    WIP

--- dist/Account.worker.js
+++ dist/Account.worker.js
@@ -4,8 +4,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _axios = require('axios');
@@ -34,23 +32,15 @@ var GitTokenAccountWorker = function () {
 
       console.log('GitToken Account Web Worker Listening for Events');
       addEventListener('message', function (msg) {
-        console.log('msg', msg);
-        console.log('typeof msg', typeof msg === 'undefined' ? 'undefined' : _typeof(msg));
-
-        var _msg$data = msg.data,
-            type = _msg$data.type,
-            value = _msg$data.value;
-
-
-        console.log('type', type);
-        console.log('value', value);
+        var _JSON$parse = JSON.parse(msg.data),
+            type = _JSON$parse.type,
+            value = _JSON$parse.value;
 
         switch (type) {
           case 'GET_PROFILE':
             return _this.getProfile({ url: value });
             break;
           case 'webpackOk':
-            console.log('Webpack setup');
             break;
           default:
             throw new Error('Invalid Type for Web Worker: ' + type);
--- src/Account.worker.js
+++ src/Account.worker.js
@@ -8,20 +8,13 @@ export default class GitTokenAccountWorker {
   listen() {
     console.log('GitToken Account Web Worker Listening for Events')
     addEventListener('message', (msg) => {
-      console.log('msg', msg)
-      console.log('typeof msg', typeof msg)
-
-      const { data: { type, value } } = msg
-
-      console.log('type', type)
-      console.log('value', value)
+      const { type, value } = JSON.parse(msg.data)
 
       switch(type) {
         case 'GET_PROFILE':
           return this.getProfile({ url: value })
           break;
         case 'webpackOk':
-          console.log('Webpack setup')
           break;
         default:
           throw new Error(`Invalid Type for Web Worker: ${type}`)
